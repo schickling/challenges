fdf3eab4a68655765d08f134c4a4d3f009bfe450
Johannes Schickling <schickling.j@gmail.com>
2014-07-15 20:00:25 +0200

    @matthiasplappert solution

--- coffee-time-challenges/03-high-product/main.go
+++ coffee-time-challenges/03-high-product/main.go
@@ -1,62 +1,26 @@
 package main
 
 import (
-	"log"
-	"math"
+	"fmt"
+	"sort"
 )
 
-type partition struct {
-	a, b []int
-}
-
 func main() {
-	x := []int{9, 8, 7, 6, 5, 4, 3, 2, 1}
-	y := permutate(x)
-	//log.Printf("%v\n", y)
-	log.Printf("Start:")
-	for _, element := range y {
-		log.Printf("%d, %d\n", assemble(element.a), assemble(element.b))
-	}
-}
+	// Idea: the product will be max. if the factors are max. To achieve this,
+	// we ensure that the factors will be as big as possible, but in a balanced way.
+	digits := []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+	x, y := 0, 0
 
-func permutate(s []int) []partition {
-	a := s
-	b := make([]int, 0)
-	l := make([]partition, 0)
-
-	for len(a) > 1 {
-		log.Printf("%v, %v", a, b)
-		for j := 0; j < len(a); j++ {
-			aa := without(a, j)
-			bb := make([]int, len(b))
-			copy(bb, b)
-			bb = append(bb, a[j])
-			l = append(l, partition{aa, bb})
-		}
-		b = append(b, a[0])
-		a = without(a, 0)
-	}
-
-	return l
-}
-
-func assemble(s []int) int {
-	x := 0
-	n := len(s)
-	for i := 0; i < n; i++ {
-		x += int(math.Pow10(n-i-1)) * s[i]
-	}
-
-	return x
-}
-
-func without(s []int, i int) []int {
-	l := make([]int, 0)
-	for k := 0; k < len(s); k++ {
-		if k != i {
-			l = append(l, s[k])
+	sort.Sort(sort.Reverse(sort.IntSlice(digits)))
+	for len(digits) > 0 {
+		digit := digits[0]
+		digits = digits[1:]
+		if x < y {
+			x = x*10 + digit
+		} else {
+			y = y*10 + digit
 		}
 	}
 
-	return l
+	fmt.Printf("%d * %d = %d\n", x, y, x*y)
 }
