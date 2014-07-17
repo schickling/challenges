2f7e9b8c83f7bbb5bb40fdfb007c41e15f625284
Johannes Schickling <schickling.j@gmail.com>
2014-07-17 18:26:39 +0200

    go solution

---
+++ coffee-time-challenges/04-exactly-a-third/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fighterlyt/permutation"
+)
+
+func main() {
+
+	digits := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 'x'}
+
+	permutation, err := permutation.NewPerm(digits, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for s, err := permutation.Next(); err == nil; s, err = permutation.Next() {
+
+		permutatedDigits := s.([]int)
+		a, b := 0, 0
+		beforeMark := true
+
+		for i := 0; i < 10; i++ {
+
+			// optimization: permutation not possible since denominator has to be 4 digits long
+			if permutatedDigits[4] != 'x' {
+				break
+			}
+
+			if i == 4 {
+				beforeMark = false
+			} else {
+				if beforeMark {
+					a = a*10 + permutatedDigits[i]
+				} else {
+					b = b*10 + permutatedDigits[i]
+				}
+			}
+
+		}
+
+		if a > 0 && b > 0 && float32(b)/float32(a) == 3 {
+			fmt.Println(a, b)
+		}
+
+	}
+
+}
