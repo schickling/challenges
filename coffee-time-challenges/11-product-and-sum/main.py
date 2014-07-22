42d0250e07ef5757370be9b25c6b392d8c764a49
Martin Thoma <info@martin-thoma.de>
2014-07-22 14:45:28 -0400

    improved speed of Python solution

--- coffee-time-challenges/11-product-and-sum/main.py
+++ coffee-time-challenges/11-product-and-sum/main.py
@@ -6,6 +6,7 @@ This should calculate
 """
 
 from itertools import permutations
+import math
 
 
 def solve_brute_force(numbers):
@@ -16,9 +17,22 @@ def solve_brute_force(numbers):
             return set([a, b])
 
 
+def solve_better(numbers):
+    """This takes 2,74s. """
+    nr_sum = sum(numbers)
+    for a in sorted(filter(lambda n: n <= math.sqrt(nr_sum), numbers),
+                    reverse=True):
+        t_sum = nr_sum - a
+        b_max = float(t_sum) / a
+        b_candidates = sorted(filter(lambda b: b <= b_max, numbers),
+                              reverse=True)
+        for b in b_candidates:
+            if a*b == nr_sum - a - b:
+                return set([a, b])
+
 if __name__ == '__main__':
     numbers = range(1, 65502+1)
-    a, b = solve_brute_force(numbers)
+    a, b = solve_better(numbers)
     print("{a} * {b} = {absum} = {sum} - {a} - {b}".format(
           a=a, b=b, absum=a+b, sum=sum(numbers)
           ))
