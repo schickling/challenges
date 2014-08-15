2ffbf32d59657a8f17442f4e525fcafbb42fcf50
Martin Thoma <info@martin-thoma.de>
2014-08-14 23:19:18 -0400

    added scala solution for 02-one-million

---
+++ coffee-time-challenges/02-one-million/main.scala
@@ -0,0 +1,7 @@
+object OneMillion {
+    def main(args: Array[String]) {
+        println("%4.0f * %4.0f = %4.0f".format(math.pow(2, 6),
+                                               math.pow(5, 6),
+                                               math.pow(10, 6)));
+    }
+}
