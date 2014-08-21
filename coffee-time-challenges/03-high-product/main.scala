07fc998a1197c634123a7feff6c204363c13ee16
Martin Thoma <info@martin-thoma.de>
2014-08-21 18:13:23 -0400

    Improved code readability

--- coffee-time-challenges/03-high-product/main.scala
+++ coffee-time-challenges/03-high-product/main.scala
@@ -1,32 +1,27 @@
 object HighProduct {
     def main(arg: Array[String]) {
-        var max_product = BigInt(0);
-        var a_max = BigInt(0L);
-        var b_max = BigInt(0L);
+        var max = List(BigInt(0), BigInt(0), BigInt(0));
         val digits = List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
         for (c <- digits.combinations(4)) {
             // Sort the digits so that the highest number gets built
             val a = c.sorted(Ordering[Int]);
             val b = (digits filterNot a.contains).sorted(Ordering[Int]);
             // calculate number a
-            var anum = BigInt(0L);
+            var anum = BigInt(0);
             for ((digit, place) <- a.zipWithIndex) {
                 anum += digit * scala.math.pow(10, place).toInt;
             }
             // calculate number b
-            var bnum = BigInt(0L);
+            var bnum = BigInt(0);
             for ((digit, place) <- b.zipWithIndex) {
                 bnum += digit * scala.math.pow(10, place).toInt;
             }
 
             // calculate number a
-            if(anum*bnum > max_product){
-                max_product = anum*bnum;
-                a_max = anum;
-                b_max = bnum;
+            if (anum * bnum > max(0)) {
+                max = List(anum * bnum, anum, bnum);
             }
         }
-
-        println("%d • %d = %d".format(a_max, b_max, max_product));
+        println("%d • %d = %d".format(max(1), max(2), max(0)));
     }
 }
