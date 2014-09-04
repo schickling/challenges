2fb770f17cca78cc6fcd02a9457d4110621f3e16
U-Alpenpunk_III\Emi <emi@eex-dev.net>
2014-09-05 00:16:33 +0200

    Put my challenge into a meaningful folder.

--- 01-dorfuchs-numbers/Scala.scala
+++
@@ -1,1 +0,0 @@
-(0 to 5).flatMap((0 to 9).drop(_).take(5).permutations.filter(_.head != 0).toSeq.distinct.map(_.mkString(""))).sortWith(_ < _).zipWithIndex.foreach(r => println((r._2 + 1) + ": " + r._1))
