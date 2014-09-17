b7c00e2455c152e033e2465eb21ce400818d87d3
Martin Thoma <info@martin-thoma.de>
2014-09-17 12:13:41 +0200

    Scala: Fixed spacing

--- coffee-time-challenges/06-word-doc/main.scala
+++ coffee-time-challenges/06-word-doc/main.scala
@@ -1,7 +1,7 @@
 object WordDoc {
     def main(arg: Array[String]) {
         val number_list = for(i <- 1 to 10000) yield i.toString;
-        val daughter_list = for(el<-number_list) yield el.replace('0', ' ');
+        val daughter_list = for(el <- number_list) yield el.replace('0', ' ');
         var sum = 0;
         for(el <- daughter_list) {
             for(n <- el.split(' ')) {
