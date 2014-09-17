814af1bf1feaf6d42b0b1c539e6720b77db9398b
Martin Thoma <info@martin-thoma.de>
2014-09-17 11:39:56 +0200

    01 TwoBases: Use one for-loop instead of three

--- coffee-time-challenges/01-two-bases/main.scala
+++ coffee-time-challenges/01-two-bases/main.scala
@@ -1,14 +1,11 @@
 object TwoBases {
-    def test(x: Int, y: Int, z: Int) = (100*x + 10*y + z == math.pow(9,2)*z + 9*y + z)
+    def test(x: Int, y: Int, z: Int) =
+        (100*x + 10*y + z == math.pow(9,2)*z + 9*y + z)
 
     def main(args: Array[String]) {
-        for( x <- 0 to 9) {
-            for( y <- 0 to 9) {
-                for( z <- 0 to 9) {
-                    if(test(x, y, z)){
-                        println("%d%d%d".format(x, y, z));
-                    }
-                }
+        for(x <- 0 to 9; y <- 0 to 9; z <- 0 to 9) {
+            if(test(x, y, z)){
+                println("%d%d%d".format(x, y, z));
             }
         }
     }
